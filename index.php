7bf4b086970da1d29a6621823326df49a671f8e0
Zukra <jelita.1331@gmail.com>
2019-10-04 12:26:23 +0300

    add show errors

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 session_start();
 $errors = (isset($_SESSION) && isset($_SESSION['errors'])) ? $_SESSION['errors'] : array();
 $success = (isset($_SESSION) && isset($_SESSION['success'])) ? $_SESSION['success'] : array();
